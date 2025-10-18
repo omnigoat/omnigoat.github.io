74aec3c38ed88955ff6c345fe904268c587e9c03
omnigoat <379771+omnigoat@users.noreply.github.com>
2025-10-18 16:51:47 +1100

    very good minimap behaviour

--- assets/js/minimap.js
+++ assets/js/minimap.js
@@ -9,14 +9,16 @@ document.addEventListener("DOMContentLoaded", () =>
 
     // width ratio from content -> minimap
     var w = 1;
-    
+
     // is mouse/touch down
     var down = !1;
     
     // last position for scrolling
     var scroll_position = 0;
-    var scroll_perc = 0.0;
     var controller_has_capture = false;
+    
+    // -1=above, 1=below, 0=within
+    var outside_minimap = 0;
 
     function reposition_controller()
     {
@@ -62,8 +64,11 @@ document.addEventListener("DOMContentLoaded", () =>
         contents.style.width = 100 / w + "%";
         contents.style.transform = `scale(${w})`;
 
+        const k = window.innerHeight / scroll_target.offsetWidth;
+        const wi = 1.0 / w;
+
         controller.style.width = minimap.clientWidth + "px";
-        controller.style.height = "120px";
+        controller.style.height = minimap.clientWidth * k + "px";
 
         align_contents();
     }
@@ -71,29 +76,37 @@ document.addEventListener("DOMContentLoaded", () =>
     function controller_down(e)
     {
         e.preventDefault();
-        controller_has_capture = true;
-        //let x = e.touches ? e.touches[0].clientX : e.clientX;
+        
         let y = e.touches ? e.touches[0].clientY : e.clientY;
 
-        // scroll percentage
-        scroll_perc = document.body.scrollTop / document.body.scrollHeight;
+        let r = minimap.getBoundingClientRect();
+        let c = controller.getBoundingClientRect();
+        
         scroll_position = y;
+        down = true;
+        outside_minimap = 0;
 
-        let r = controller.getBoundingClientRect();
-        if (y < r.top)
+        // mouse on controller, we're dragging
+        if (c.top <= y && y <= c.bottom)
         {
-            y = y / 2 + r.top / 2;
+            controller_has_capture = true;
+            return;
         }
+        // move to very top
+        else if (y < r.top + c.height / 2)
+        {
+            window.scrollTo(0, 0);
+        }
+        // move to very bottom
+        else if (y > r.bottom - c.height / 2)
+        {
+            window.scrollTo(0, document.body.scrollHeight);
+        }
+        // move arbitrarily elsewhere
         else
         {
-            if (!(y > r.bottom))
-                return void (down = !0);
-            y = y / 2 + r.top / 2
+            window.scrollTo(0, (y - c.height / 2) / r.height * document.body.scrollHeight);
         }
-        scroll_position = y;
-        down = i.top < g && i.bottom > g;
-        let d = y / window.innerHeight * document.body.clientHeight;
-        window.scrollTo(0, d)
     }
 
     function controller_move(e)
@@ -101,7 +114,7 @@ document.addEventListener("DOMContentLoaded", () =>
         const x = e.touches ? e.touches[0].clientX : e.clientX;
         const y = e.touches ? e.touches[0].clientY : e.clientY;
         let c = controller.getBoundingClientRect();
-        let r  = minimap.getBoundingClientRect();
+        let r = minimap.getBoundingClientRect();
 
         // colourize the controller
         if (down || c.left <= x && x <= c.right && c.top <= y && y <= c.bottom)
@@ -119,47 +132,50 @@ document.addEventListener("DOMContentLoaded", () =>
 
         // don't perform drag scrolling if we weren't down
         if (!down)
-            return;
-        e.preventDefault();
-        
-        // don't perform scrolling if we were outside the minimap
-        if (y < r.top || r.bottom < y)
         {
-            controller_has_capture = false;
             return;
         }
-
-        // update mouse-delta of drag
-
-        var delta = (y - scroll_position);
-        scroll_position = y;
-
-        // ensure recapture occurs within the middle two-quarters
+        e.preventDefault();
+        
+        // ensure recapture occurs within the middle
         if (!controller_has_capture)
         {
-            const c1qt = c.top + c.height / 4;
-            const c3qt = c.top + c.height * 3 / 4;
+            const c1qt = c.top + c.height / 2.1;
+            const c3qt = c.bottom - c.height / 2.1;
 
-            if (y <= c3qt && c1qt <= y)
+            if (outside_minimap === -1 && y > c1qt ||
+                outside_minimap === 1 && y < c3qt ||
+                outside_minimap === 0)
             {
                 controller_has_capture = true;
             }
             else
             {
+                scroll_position = y;
                 return;
             }
         }
 
-        const yt = y - minimap.clientTop;
-        const sr = r.height + c.height;
-        const dd = yt / sr;
-        //window.scrollTo(0, dd * document.body.scrollHeight);
+        // don't perform scrolling if we were outside the minimap
+        if (y < r.top || r.bottom < y)
+        {
+            controller_has_capture = false;
+            outside_minimap = (y < r.top ? -1 : r.bottom < y ? 1 : 0);
+            return;
+        }
+
+        // update mouse-delta of drag
+        var delta = (y - scroll_position);
         window.scrollBy(0, delta / r.height * document.body.scrollHeight);
+
+
+        scroll_position = y;
     }
 
     function controller_up()
     {
         down = !1;
+        controller_has_capture = false;
     }
 
     function mouseleave()
@@ -170,8 +186,8 @@ document.addEventListener("DOMContentLoaded", () =>
     window.addEventListener("resize", resize_minimap);
     window.addEventListener("scroll", align_contents);
 
-    controller.addEventListener("mousedown", controller_down);
-    controller.addEventListener("touchdown", controller_down);
+    minimap.addEventListener("mousedown", controller_down);
+    minimap.addEventListener("touchdown", controller_down);
 
     window.addEventListener("mousemove", controller_move);
     window.addEventListener("touchmove", controller_move);
